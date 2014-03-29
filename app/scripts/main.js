bacc06f9b6ebf34a929d73d2a8346279f4f71f0f
Martin van Houte <mvhoute@gmail.com>
2014-03-29 22:32:45 +0100

    Added sounds + changed buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+function play(){
+    var audio = document.getElementById("audio");
+    audio.play();
+}
+
+function playSound(el,soundfile) {
+    if (el.mp3) {
+        if(el.mp3.paused) el.mp3.play();
+        else el.mp3.pause();
+    } else {
+        el.mp3 = new Audio(soundfile);
+        el.mp3.play();
+    }
+}
